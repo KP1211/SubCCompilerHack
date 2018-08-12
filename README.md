bb83fc5874079871b3b1a67034b10c390405d42c
yizongk <yizongk@gmail.com>
2018-08-12 18:19:12 -0400

    Updated Ninja version to be semi-quine-able with a bug. Upadated README to contain new info about bug and what this current version has. Removed some unnessary binary

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ This repository contains modified version of the subc compiler.
 Me and my team's goal is to hack this compiler to do the following things:
  - some 
 
+# -----BUGS---------
+
+## *Quine only works on first two iteration*
+ - $ gcc -o test-scc *.c 
+ - $ ./test-scc -o more-scc *.c
+ - $ ./more-scc -o extreme-scc *.c   --> At this point, it causes error, string quine is not of correct format.
+
 # -----PROGRESS-----
 
 ## *Good things to note*
@@ -22,12 +29,14 @@ Me and my team's goal is to hack this compiler to do the following things:
  	- 4. 'decl.c'
  	- 5. 'decl.h'
  	- 6. 'data.h'
-    - 7. 'def.h'
+  - 7. 'def.h'
 	- (Look for '//****************', those are the places where I have added on to the default source code, some default codes are commented out near these indication.)*
+ - In a clean subc src file, you must do '$ make all' in terminal, so that some prerequiste file are compiled by gcc before you get the first iteration of scc to compile.
 	
 ## **The idea**
  - Instead of reading character by character from a FILE variable and then compiling, we change it to reading character by charater from a String variable and then compiling.
  - After that, we will now have a way to store our malicious codes within the program and have it compile at ANY time while not leaving any noticable breadcrumbs, except in binary form.
+ - From then on, we make our compile source code a quine so that it can reproduce it self in future iterations.
 
 ## **What's done (This apply only to Ninja version, I have not documented Breadcrumb version.)**
  - What's currently on github, once compiled, will produce a program is coded to read .c source file as a FILE variable and then be converted to a String variable, character by character. After that, our program will read character by character from the String variable containing our input-source-code, character by character, and then compiling.
@@ -38,6 +47,9 @@ Me and my team's goal is to hack this compiler to do the following things:
   - Include() is now edited to work with Strings instead of FILE.
    - *There may be more places where Infiles is being modified, but so far it's working the way it is right now.* 
  - Now will scan all input sources and replace all instances of "Hello" to "Good bye". Codes is in main.c.
+ - Maximun string length has been increased to 200,000.
+ - Has a string variable to contain the complete source code of the 7 files that I have modified (specified in section *Good things to note*). 
+ - Has a source code that is quine-able, and some for loops to make it a quine.
 
 *Essentially I have modified nothing but the source of input (With a String variable and some support int variables to imitate how type FILE works). The rest of the program will work as usual.*
 
@@ -46,6 +58,12 @@ Me and my team's goal is to hack this compiler to do the following things:
  - 1. **char \*Insource** for Holding content - *A String variable to hold content of source files instead of using a FILE variable. The difference being that String has the content internally inside the program in the RAM, while FILE points to contents outside the program such as the HDD/SSD and does not retain a copy of the source file inside the program in the RAM.*
  - 2. **int Insourcei** for keeping track - *An index to keep track of what character to parse next in the String, while FILE variable is a pointer and is able to keep track of it self.*
  - 3. **int Insourcelen** for EOF (end of file) signal - *A String length is nesscary to imitate EOF (which is -1 in interger form) in a String, because for some reason, string cannot hold an Ascii value of -1, but a primative type char is able to. So that being said, when the parser hits string length, it will return -1 (EOF).*
+
+ ## **How I made it a quine**
+ - 1. String variable, call it S, in main.c that stored main.c in an one line form (Using a preprocess .c file that is included in this github repository).
+ - 2. S is then split into two, the first half is to be right after the opening double quote and the second half is to be right before the ending double quote. If the part of the source code containing S is '... s = ""; ...' then S.firsthalf is '... s ="' and S.secondhalf is '"; ...'.
+ - 3. Now we find the the index where the half is splited, insert an escape version of S.firsthalf and then an escaped version fo S.secondhalf, so now we have S = S.firsthalf + S.escapedfirsthalf + S.escapedsecondhalf + S.secondhalf.
+ - 4. Now If main.c is the input file being compiled, instead of passing the normal main.c to the rest of the program, pass S to the rest of the program.
 
 ## **Direction**
  - **Breadcrumbs verison**
@@ -64,5 +82,3 @@ Me and my team's goal is to hack this compiler to do the following things:
   - $ ../../helloworld.exe
 
 ## *More coming soon to your local repository in the end of summer...*
-
-got to make all on src to compile some prerequiste files.
--- preprocess_source_code/c_oneline_preprocess.c
+++ preprocess_source_code/c_oneline_preprocess.c
@@ -5,8 +5,100 @@
     // Might cause bug otherwise.
 
 // Removes all comments.
-// Replace newlines, tabs and space.
-// For Preprocessor commands, it will give it its own line.
+// Escape single quote, double quote, and back slash.
+// Replace new line and tabs with two char in a string, to make it oneline.
+
+static int maxstrlen;
+
+void removecomment(char *source, int *sourcesize) {
+    int j, l, k;
+    
+    for( j = 0, l = 0; j < *sourcesize - 1; ++j ) {
+        if( source[j] == '/' ) {
+            l = j;
+            if( source[l+1] == '/' ) {
+                for( k = l + 2; (k < *sourcesize) && (source[k] != '\n'); ++k ) { } // To get to the end of comment. k = j + 2 cuze j and j+1 are already parsed to be //.
+                for( ; k < *sourcesize; ++l, ++k ) {
+                    source[l] = source[k];
+                }
+                source[l] = '\0';
+                *sourcesize = strlen(source);
+                continue;
+            }
+            if( source[l+1] == '*' ) {
+                for( k = l + 2; (k < *sourcesize - 1) && ( (source[k] != '*') || (source[k+1] != '/') ); ++k ) { } // To get to the end of comment. k = j + 2 cuze j and j+1 are already parsed to be /*.
+                k = k + 2;      // This will be anything after */ since k and k+1 is */.
+                for( ; k < *sourcesize; ++l, ++k ) {
+                    source[l] = source[k];
+                }
+                source[l] = '\0';
+                *sourcesize = strlen(source);
+                continue;
+            }
+        }
+    }
+    *sourcesize = strlen(source);
+
+    return;
+}
+
+void oneline( char *source, int *sourcesize, char *filename ) {
+    int i, j;
+
+    for( i = 0; i < *sourcesize ; ++i ) {
+        if( source[i] == '\n' || source[i] == '\t' ) {
+            for( j = *sourcesize; (j < maxstrlen) && (j > i); --j ) {     
+                if( j == *sourcesize ) 
+                    source[j+1] = '\0';     
+                source[j] = source[j-1];
+            }
+            if( source[i+1] == '\t' ) {
+                source[i+1] = 't';
+            } else if ( source[i+1] == '\n' ) {
+                source[i+1] = 'n';
+            } else {
+                printf("Unrecognized character: %c", source[j]);
+            }
+            source[i] = '\\';
+            
+            *sourcesize = strlen(source);
+            ++i;    // To move the index past what is already processed.
+            if( strlen(source) >=  maxstrlen ) {
+                printf("In-program-array may not be allocated enough to contain all nesccary new characters. Changed in-program-array to be allocated more space. Skipping '%s'.\n", filename);
+                break;
+            }
+        }
+    }
+
+    return;
+}
+
+void escapesequence( char *source, int *sourcesize , char *filename) {
+    int j, l;
+    char c;
+
+    for( j = 0, l = 0; j < *sourcesize; ) {
+        c = source[j];
+        ++j;
+
+        if( c == '\\' || c == '\'' || c == '\"' ) {
+            for( l = *sourcesize; (l < maxstrlen) && (l > j-1); --l ) {     
+                if( l == *sourcesize ) 
+                    source[l+1] = '\0';     
+                source[l] = source[l-1];
+            }
+            source[l] = '\\';
+            *sourcesize = strlen(source);
+            if( strlen(source) >=  maxstrlen ) {
+                printf("In-program-array may not be allocated enough to contain all nesccary new characters. Changed in-program-array to be allocated more space. Skipping '%s'.\n", filename);
+                break;
+            }
+            ++j;    // To continue on stuff that not parsed, meaning after \\ or \' or \"
+        }
+    }
+
+    return;
+}
 
 void main( int argc, char *argv[] ) {
     int i, j, k, l, sourcesize;
@@ -16,7 +108,7 @@ void main( int argc, char *argv[] ) {
     char tmpword[100];
     char source[200000];
     char c;
-    int maxstrlen;
+    
 
     if( argc < 2 ) {
         printf("Expecting 1 or more argument\n");
@@ -25,7 +117,6 @@ void main( int argc, char *argv[] ) {
     
     maxstrlen = 200000;
     for( i = 1; i < argc; ++i ) {
-        //printf("i: %d - '%s'\n",i,argv[i]);
         if( !strcmp(argv[1],"-o") ) {
             if( argc < 4 ) {
                 printf("Expecting 2 or more argument after '-o'\n");
@@ -59,7 +150,6 @@ void main( int argc, char *argv[] ) {
         strcpy(source,"");
 
 // At this point, all filenaming issue have been resolve.
-
         // have a storage char variable for whole source.
         for( j = 0; j < maxstrlen && ((c = fgetc(input)) != EOF); ++j ) {
             source[j] = c;
@@ -68,119 +158,28 @@ void main( int argc, char *argv[] ) {
         sourcesize = strlen(source);
         printf("Parsing input file '%s'\n",argv[i]);
         printf("clean sourcelen: %d\n",sourcesize);
+        
+//***** Delete all comments. 
 
-        //***** Delete all comments. 
-        for( j = 0, l = 0; j < sourcesize - 1; ++j ) {
-            if( source[j] == '/' ) {
-                l = j;
-                if( source[l+1] == '/' ) {
-                    for( k = l + 2; (k < sourcesize) && (source[k] != '\n'); ++k ) { } // To get to the end of comment. k = j + 2 cuze j and j+1 are already parsed to be //.
-                    for( ; k < sourcesize; ++l, ++k ) {
-                        source[l] = source[k];
-                    }
-                    source[l] = '\0';
-                    sourcesize = strlen(source);
-                    continue;
-                }
-                if( source[l+1] == '*' ) {
-                    for( k = l + 2; (k < sourcesize - 1) && ( (source[k] != '*') || (source[k+1] != '/') ); ++k ) { } // To get to the end of comment. k = j + 2 cuze j and j+1 are already parsed to be /*.
-                    k = k + 2;      // This will be anything after */ since k and k+1 is */.
-                    for( ; k < sourcesize; ++l, ++k ) {
-                        source[l] = source[k];
-                    }
-                    source[l] = '\0';
-                    sourcesize = strlen(source);
-                    continue;
-                }
-            }
-        }
-        sourcesize = strlen(source);
-        //****/
-        printf("sourcelen: %d\n",sourcesize);
-        printf("-------------------------------------------\n");
-        //for( int i = 0; i < strlen(source); ++i ) {
-        //    printf("%c",source[i]);
-        //}
+        removecomment( source, &sourcesize );
+
+// At this point, string should be a one liner.
+
+        escapesequence( source, &sourcesize, argv[i] );  
 
 // At this point, all comments have been removed.
 
-        for( j = 0, l = 0; j < sourcesize; ) {
-            c = source[j];
-            ++j;
-            // vvv Escaping sequences vvv
-            if( c == '\\' || c == '\'' || c == '\"' ) {
-                for( l = sourcesize; (l < maxstrlen) && (l > j-1); --l ) {     // Everything on l and after are moved over to the right once slot. Starting at the end of the c-string iterating to j (iterarting from right to left in the array).
-                    if( l == sourcesize ) 
-                        source[l+1] = '\0';     // Should only run once, at the first iteration of the loop, to make sure new string is proper c-string.
-                    //if(l<200) printf("Replacing source[%d]: '%c' with source[%d] '%c'\n",l,source[l], l-1,source[l-1]);
-                    source[l] = source[l-1];
-                }
-                source[l] = '\\';
-                //printf("start: '%c'\n",source[j+1]);
-                sourcesize = strlen(source);
-                if( strlen(source) >=  maxstrlen ) {
-                    printf("In-program-array may not be allocated enough to contain all nesccary new characters. Changed in-program-array to be allocated more space. Skipping '%s'.\n", argv[i]);
-                    break;
-                }
-                ++j;    // To continue on stuff that not parsed, meaning after \\ or \' or \"
-            }
-            // ^^^ Escaping sequences ^^^
+        oneline( source, &sourcesize, argv[i] );
 
-        }
 
 // At this point, all characters that need to be escaped has been escaped.
 
-        for( j = 0; j < sourcesize; ++j ) {
-            c = source[j];
+        printf("sourcelen after preprocess: %d\n",sourcesize);
+        printf("-------------------------------------------\n");
+                
 
-            // vvv Fix spacing issues vvv
-            if(c == '\n' || c == '\t' ) {         // Remove all new lines and tabs with space.
-                source[j] = ' ';
-            } 
-            // ^^^ Fix spacing issues ^^^
-
-            //* vvv Dealing with # commands vvv This is special case, as it skips the rest of the loop if this part runs.
-            if( c == '#' ) {            // Used to parse anything that starts with '#'
-                for( l = sourcesize; (l < maxstrlen) && (l > j); --l ) {     // Everything on l and after are moved over to the right once slot. Starting at the end of the c-string iterating to j (iterarting from right to left in the array).
-                    if( l == sourcesize ) 
-                        source[l+1] = '\0';     // Should only run once, at the first iteration of the loop, to make sure new string is proper c-string.
-                    source[l] = source[l-1];
-                }
-                source[l] = '\n';
-                sourcesize = strlen(source);
-                ++j;
-                for( ; source[j] != '\n'; ++j ) {
-
-                }
-            }
-            // ^^^ Dealing with # commands ^^^ */
-        }
-
-// At this point, source should be rid of all unnessary charaters. 
 //printf("%s\n",source);
-        /***vvvvv making it one line vvvvv
-        for( j = 0, l = 0; j < sourcesize; ) {
-            c = source[j];
-            ++j;
-            if( c == '\n' ) {
-                for( l = sourcesize; (l < maxstrlen) && (l > j); --l ) {     // Everything on l and after are moved over to the right once slot. Starting at the end of the c-string iterating to j (iterarting from right to left in the array).
-                    if( l == sourcesize ) 
-                        source[l+1] = '\0';     // Should only run once, at the first iteration of the loop, to make sure new string is proper c-string.
-                    source[l] = source[l-1];
-                }
-                source[l-1] = '\\';
-                source[l] = 'n';
-                sourcesize = strlen(source);
-                if( strlen(source) ==  maxstrlen ) {
-                    printf("In-program-array may not be allocated enough to contain all nesccary new characters. Changed in-program-array to be allocated more space. Skipping '%s'.\n", argv[i]);
-                    break;
-                }
-                ++j;    // To continue on stuff that not parsed, meaning after \n
-            }
-        }
-        //^^^^^ making it one line ^^^^^ ***/
 printf("%s\n",source);
-// At this point, source should be a one liner.
 
         //vvvvv outputing to file vvvvv
         fputs(source, output);
